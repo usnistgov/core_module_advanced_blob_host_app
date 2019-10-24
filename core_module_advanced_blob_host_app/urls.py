7c2a40f3e979373426e963f90a21f6146ca46f85
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:23 -0400

    fix: revert django upgrade
    Change-Id: I33028b15eae7e3f85b3a1d11ff36fdcc65b64f00

--- core_module_advanced_blob_host_app/urls.py
+++ core_module_advanced_blob_host_app/urls.py
@@ -1,10 +1,9 @@
 """ Url router for the advanced blob host module
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_advanced_blob_host_app.views.views import AdvancedBlobHostModule
 
 urlpatterns = [
-    re_path(r'module-advanced-blob-host', AdvancedBlobHostModule.as_view(), name='core_module_advanced_blob_host_view'),
+    url(r'module-advanced-blob-host', AdvancedBlobHostModule.as_view(), name='core_module_advanced_blob_host_view'),
 ]
